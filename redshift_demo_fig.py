706518c3594a544404dffdbc22423fbb1c24ae08
srodney <steve.rodney@gmail.com>
2015-05-05 15:50:20 -0400

    cleaned up getredshift.py and converted to redshift_demo_fig.py * this module's only purpose is to make the med band demo figure (fig 1 in the paper)

--- redshift_demo_fig.py
+++ redshift_demo_fig.py
@@ -5,10 +5,11 @@ from matplotlib import pyplot as pl
 import numpy as np
 from scipy import interpolate as scint
 
-IIPlist = ['s11-2004hx','s11-2005lc','s11-2005gi','s11-2006jl']
-Ibclist = ['s11-2005hl','s11-2005hm','s11-2006fo','s11-2006jo']
-
-def mkdemofig( showcc=False ):
+def mkdemofig():
+    """ Construct a demonstration figure showing how the medium band filters
+    pick out features in the Type Ia SN spectrum and how pseudo-colors can
+    provide redshift information.  (This is Figure 1 in the paper)
+    """
     from matplotlib import ticker
     from pytools import plotsetup
     plotsetup.fullpaperfig(figsize=[8,4])
@@ -27,7 +28,6 @@ def mkdemofig( showcc=False ):
     ax3.set_xlim( 1.12, 1.69 )
     ax3.set_ylim( 0.0, 0.7 )
     ax3.set_yticklabels([])
-    #ax3.set_xticks([1.1,1.3,1.5,1.7])
     ax3.set_xlabel('wavelength ($\mu$m)')
     ax2.set_ylabel('SN Flux or Filter Transmission\n (arbitrary units)')
 
@@ -40,12 +40,8 @@ def mkdemofig( showcc=False ):
     ax3.text(0.95,0.55,'z=2.2', color='darkred', transform=ax3.transAxes,ha='right',va='top')
 
     ax4 = pl.subplot( 2, 2, 2 )
-    mkcirclepoints(zrange=[1.8,2.2], colorselect=[1,0], source='hsiao', marker='o', ls=' ', mew=0 )
-    if showcc :
-        for source in IIPlist :
-            mkcircleline(zrange=[1.8,2.2], colorselect=[1,0], source=source, color='b',marker='o', alpha=0.2, ls=' ', lw=1 )
-        for source in Ibclist :
-            mkcircleline(zrange=[1.8,2.2], colorselect=[1,0], source=source, color='g',marker='o', alpha=0.2, ls=' ', lw=1 )
+    mkcirclepoints(zrange=[1.8,2.2], colorselect=[1,0], source='hsiao',
+                   marker='o', ls=' ', mew=0.1, mec='w' )
     ax4.text( 0.19, -0.13, 'z=1.8', color='darkblue', ha='right', va='center', rotation=0)
     ax4.text( -0.06, 0.02, 'z=2.0', color='green', ha='center', va='center', rotation=0)
     ax4.text( 0.15, -0.34, 'z=2.2', color='darkred', ha='left', va='center', rotation=0)
@@ -57,18 +53,14 @@ def mkdemofig( showcc=False ):
     ax4.yaxis.set_label_position( 'right')
 
     ax5 = pl.subplot( 2, 2, 4, sharex=ax4 )
-    mkcirclepoints(zrange=[1.8,2.2], colorselect=[1,2], source='hsiao', marker='o', ls=' ', mew=0 )
-    if showcc :
-        for source in IIPlist :
-            mkcircleline(zrange=[1.8,2.2], colorselect=[1,2], source=source, color='b',marker='o', alpha=0.2, ls=' ', lw=1 )
-        for source in Ibclist :
-            mkcircleline(zrange=[1.8,2.2], colorselect=[1,2], source=source, color='g',marker='o', alpha=0.2, ls=' ', lw=1 )
+    mkcirclepoints(zrange=[1.8,2.2], colorselect=[1,2], source='hsiao',
+                   marker='o', ls=' ', mew=0.1, mec='w' )
 
     ax5.yaxis.set_ticks_position( 'right')
     ax5.yaxis.set_ticks_position( 'both')
     ax5.yaxis.set_label_position( 'right')
 
-    ax5.text( 0.21, 0.04, 'z=1.8', color='darkblue', ha='center', va='center', rotation=0)
+    ax5.text( 0.2, 0.02, 'z=1.8', color='darkblue', ha='center', va='center', rotation=0)
     ax5.text( -0.07,-0.09, 'z=2.0', color='green', ha='center', va='center', rotation=0)
     ax5.text( 0.11, 0.13, 'z=2.2', color='darkred', ha='center', va='center', rotation=0)
 
@@ -95,7 +87,6 @@ def mkdemofig( showcc=False ):
 def mksedplot( z=1.8, color='k' ):
     """ make a set of plots showing the SN Ia SED (Hsiao template)
     at various redshifts, with bandpasses overlaid.
-    :return:
     """
     snIa = sncosmo.Model( source='hsiao' )
     snIa.set( z=z, t0=0 )
@@ -106,11 +97,11 @@ def mksedplot( z=1.8, color='k' ):
 
     pl.plot( snwave, snflux, color=color, ls='-')
 
-    f105w = sncosmo.get_bandpass( 'wfc3f105w' )
-    f098m = sncosmo.get_bandpass( 'wfc3f098m' )
-    f127m = sncosmo.get_bandpass( 'wfc3f127m' )
-    f139m = sncosmo.get_bandpass( 'wfc3f139m' )
-    f153m = sncosmo.get_bandpass( 'wfc3f153m' )
+    f105w = sncosmo.get_bandpass( 'f105w' )
+    f098m = sncosmo.get_bandpass( 'f098m' )
+    f127m = sncosmo.get_bandpass( 'f127m' )
+    f139m = sncosmo.get_bandpass( 'f139m' )
+    f153m = sncosmo.get_bandpass( 'f153m' )
 
     wf127m = f127m.wave / 10000.
     wf139m = f139m.wave / 10000.
@@ -135,62 +126,39 @@ def mksedplot( z=1.8, color='k' ):
 
 
 
-def mkcircleline(zrange=[1.8,2.2], t0=0, colorselect=[0,1],
-                 source='hsiao', **plotargs ):
-    f127m, f139m, f153m = [], [], []
-    f125w, f140w, f160w = [], [], []
+def mkcirclepoints(zrange=[1.8,2.2], t0=0, colorselect=[0,1],
+                   source='hsiao', **plotargs ):
+    """ Make a string of colored points in pseudo-color-color space,
+    showing where a fiducial Type Ia SN lands as a function of redshift.
+    """
+    import mpltools
+    from matplotlib import cm
+    from copy import copy
+    labelcolors = ['darkmagenta','teal','darkorange']
+    plotargs10 = copy(plotargs)
+    plotargs10.update( mec='w', marker='D', zorder=1000, ms=10 )
+
+    zsteps = np.arange( zrange[0], zrange[1], 0.01 )
+    mpltools.color.cycle_cmap( length=len(zsteps), cmap=cm.jet )
     for z in np.arange( zrange[0], zrange[1], 0.01 ) :
         sn = sncosmo.Model( source=source )
         sn.set( z=z, t0=t0 )
-        f127m.append( sn.bandmag('wfc3f127m', 'ab', 0.) )
-        f139m.append( sn.bandmag('wfc3f139m', 'ab', 0.) )
-        f153m.append( sn.bandmag('wfc3f153m', 'ab', 0.) )
-        f125w.append( sn.bandmag('wfc3f125w', 'ab', 0.) )
-        f140w.append( sn.bandmag('wfc3f140w', 'ab', 0.) )
-        f160w.append( sn.bandmag('wfc3f160w', 'ab', 0.) )
-    med = np.array([ f127m,f139m,f153m] )
-    broad = np.array([ f125w,f140w,f160w] )
-    color = med-broad
-    pl.plot( color[colorselect[0]], color[colorselect[1]], **plotargs )
-
-    colorlabel = ['F127M-F125W','F139M-F140W','F153M-F160W']
-    ax = pl.gca()
-    ax.set_xlabel(colorlabel[colorselect[0]])
-    ax.set_ylabel(colorlabel[colorselect[1]])
-
-
-
-
-def classifyDemo(zrange=[1.8,2.2] ):
-    """ Make a demo figure showing the classification power.
-    :param zrange:
-    :param colorselect:
-    :param source:
-    :param plotargs:
-    :return:
-    """
-    from mpltools import color
-    from matplotlib import cm
-
-    labelcolors = ['darkmagenta','teal','darkorange']
-
-    zsteps = np.arange( zrange[0], zrange[1], 0.01 )
-    color.cycle_cmap( length=len(zsteps), cmap=cm.jet )
-
-    for z in zsteps :
-        #for x1 in x1steps :
-        sn = sncosmo.Model( source='salt2-extended' )
-        sn.set( z=z, t0=0 )
-        f127m = sn.bandmag('wfc3f127m', 'ab', 0.)
-        f139m = sn.bandmag('wfc3f139m', 'ab', 0.)
-        f153m = sn.bandmag('wfc3f153m', 'ab', 0.)
-        f125w = sn.bandmag('wfc3f125w', 'ab', 0.)
-        f140w = sn.bandmag('wfc3f140w', 'ab', 0.)
-        f160w = sn.bandmag('wfc3f160w', 'ab', 0.)
+        f127m = sn.bandmag('f127m', 'ab', 0.)
+        f139m = sn.bandmag('f139m', 'ab', 0.)
+        f153m = sn.bandmag('f153m', 'ab', 0.)
+        f125w = sn.bandmag('f125w', 'ab', 0.)
+        f140w = sn.bandmag('f140w', 'ab', 0.)
+        f160w = sn.bandmag('f160w', 'ab', 0.)
         med = np.array([ f127m,f139m,f153m] )
         broad = np.array([ f125w,f140w,f160w] )
-        color = med-broad
-        pl.plot( color[colorselect[0]], color[colorselect[1]], **plotargs )
+        pseudocolor = med-broad
+        colorx = pseudocolor[colorselect[0]]
+        colory = pseudocolor[colorselect[1]]
+
+        if (int(np.round(z*100))%10) == 0 :
+            pl.plot( colorx, colory, **plotargs10 )
+        else :
+            pl.plot( colorx, colory, **plotargs )
 
     colorlabel = ['F127M-F125W','F139M-F140W','F153M-F160W']
     ax = pl.gca()
